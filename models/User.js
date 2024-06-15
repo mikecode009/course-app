faf3ea8c79e81f1aace3176f90b03e38d2381381
mikecode009 <78647644+mikecode009@users.noreply.github.com>
2024-06-15 15:47:02 +0200

    xcxc

--- models/User.js
+++ models/User.js
@@ -3,10 +3,11 @@ const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, 
+     required: true },
   favourites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
 });
-
+ 
 userSchema.pre('save', async function(next) {
   if (this.isModified('password')) {
     this.password = await bcrypt.hash(this.password, 8);
